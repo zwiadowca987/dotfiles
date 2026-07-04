1926947d4f2ad732ac256948e8476d6cf5d8a08d
zwiadowca987 <mat.szczerek@gmail.com>
2026-07-04 18:49:02 +0200

    scroll upgrade

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map({ "n", "v", "i" }, "<ScrollWheelUp>", "<C-Y>", { desc = "Przewiń do góry o 1 linijkę" })
+map({ "n", "v", "i" }, "<ScrollWheelDown>", "<C-E>", { desc = "Przewiń w dół o 1 linijkę" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
